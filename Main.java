26fe77b2253c1c71a32e37d430f18bdd651b81e1
anandkhera <anandk3@nycstudents.net>
2024-10-09 15:24:19 +0000

    reformatted

--- Main.java
+++ Main.java
@@ -5,7 +5,40 @@ public class Main
         Feeder f = new Feeder();
         System.out.println(f);
   //      System.out.println(f.currentFood);
-        System.out.print(f.getCurrentFood());
+        System.out.println(f.getCurrentFood());
         Feeder g = new Feeder (500);
+        System.out.println(g.getCurrentFood());
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+        System.out.println(Math.random());
+        double random= Math.random() * 10;
+        System.out.println(random);
+        random = (int) (Math.random()) * 10;
+        System.out.println(random);
+        random = (int)(Math.random() * 10 + 1);
+        System.out.println(random);
+        System.out.println(Math.random());
+        random= Math.random() * 10;
+        System.out.println(random);
+        random = (int) Math.random() * 10;
+        System.out.println(random);
+        random = (int)(Math.random() * 10);
+        System.out.println(random);
+        random = (int)(Math.random() * 10 + 1);
+        System.out.println(random);
+        System.out.println(Math.random() < 0.05);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+        Feeder k = new Feeder(0);
+        System.out.println(k.simulateManyDays(5, 10));
+
+                
     }
 }
